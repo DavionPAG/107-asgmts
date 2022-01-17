05573e81aa598fd5ace95d94091e58078f8a6ee5
Davion <daviongarcia@gmail.com>
2022-01-17 15:39:53 -0800

    useContext

--- react-basics/src/components/Home.js
+++ react-basics/src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './home.css'
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className='home-main'>
+      <img src="/media/logo.svg" alt="logo" className='logo'/>
     </div>
   )
 }
--- react-basics/src/components/Navbar.js
+++ react-basics/src/components/Navbar.js
@@ -1,34 +1,36 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom'
 import store from './context/store';
+import './navbar.css'
 
 export default function Navbar() {
   const cart = useContext(store).cart
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "rgb(147, 250, 178)"}}>
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" to='/'>Home</Link>
+              <Link className="nav-link" to='/'>Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/catalog">Catalog</Link>
             </li>
-            
+
             <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li>
 
           </ul>
-          <div className="d-flex">
-              <Link class="btn btn-primary btn-outline btn-outline-success bg-black form-control me-2" to="/cart">
-                Cart
-                <span className='badge bg-primary'>{
-                cart.length} {console.log(cart)}</span>
-              </Link>              
+          <div className="d-flex form-inline my-2 my-lg-0">
+            <Link class="btn btn-dark btn-outline btn-outline-primary form-control mr-sm-2" to="/cart">
+              <p>Cart </p>
+              <span className="badge badge-light">
+                {cart.length} {console.log(cart)}
+              </span>
+            </Link>
           </div>
         </div>
       </div>
